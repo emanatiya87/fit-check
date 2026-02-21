4e1cff4cef6eba8a42c835afcc3631955979d9af
emanatiya87 <emanatiya87@gmail.com>
2026-02-21 02:34:56 +0200

    footer complete links

--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -1,5 +1,6 @@
 import Title from "./title";
 import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 export default function LayoutFooter() {
   return (
@@ -11,15 +12,24 @@ export default function LayoutFooter() {
         <Title titleText="اطلبي بسهولة على واتساب" color="secondary" />
       </div>
       <p className="mt-4 text-secondary font-semibold">تواصلي معنا مباشرة</p>
-
-      <button
-        type="button"
-        className="mt-4 text-light bg-[#546E3C] border border-[#546E3C] 
-                  rounded-4xl font-semi-bold text-md px-5 py-2.5 flex items-center gap-1 m-auto"
+      <a
+        href="https://wa.me/201113364852?text=hello%20Fit%20Check%20i%20want%20to%20order"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-fit mt-4 text-light bg-[#546E3C] border border-[#546E3C] 
+             rounded-4xl font-semi-bold text-md px-5 py-2.5 flex items-center gap-1 m-auto"
       >
         <FaWhatsapp />
         اطلبي على الواتساب
-      </button>
+      </a>
+      <a
+        target="_blank"
+        href="https://www.facebook.com/groups/750706561027930/?ref=share&mibextid=NSMWBT"
+        className="w-fit mt-4 text-light bg-[#0064E0] border border-[#546E3C] 
+             rounded-4xl font-semi-bold text-md px-5 py-2.5 flex items-center gap-1 m-auto"
+      >
+        <FaFacebook /> جروب
+      </a>
     </div>
   );
 }
