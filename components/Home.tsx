18f517a079a25328a3a8489d1c172b398823601c
emanatiya87 <emanatiya87@gmail.com>
2026-02-21 10:12:59 +0200

    create home for separet layout

---
+++ components/Home.tsx
@@ -0,0 +1,12 @@
+import Banner from "./Banner";
+import LayoutFooter from "./LayoutFooter";
+import Products from "./Products";
+export default function Home() {
+  return (
+    <>
+      <Banner />
+      <Products />
+      <LayoutFooter />
+    </>
+  );
+}
