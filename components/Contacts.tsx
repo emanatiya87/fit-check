e8cb4d769177a3f6059ef5d9120301f2fb9c760d
emanatiya87 <emanatiya87@gmail.com>
2026-02-21 13:46:29 +0200

    unneeded imports

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,60 +1,17 @@
 "use client";
 import Image from "next/image";
-import {
-  Footer,
-  FooterBrand,
-  FooterCopyright,
-  FooterDivider,
-  FooterIcon,
-  FooterLink,
-  FooterLinkGroup,
-  FooterTitle,
-} from "flowbite-react";
-import {
-  BsDribbble,
-  BsFacebook,
-  BsGithub,
-  BsInstagram,
-  BsTwitter,
-} from "react-icons/bs";
+import { Footer, FooterDivider, FooterIcon } from "flowbite-react";
+import { BsFacebook, BsInstagram } from "react-icons/bs";
 
 export function Contacts() {
   return (
     <Footer container>
       <div className="w-full">
-        {/* <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
-          <div>
-            <FooterBrand href="/" src="/logo.png" alt="Fit Check Logo" />
-          </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
-            <div>
-              <FooterTitle title="about" />
-              <FooterLinkGroup col>
-                <FooterLink href="#">Flowbite</FooterLink>
-                <FooterLink href="#">Tailwind CSS</FooterLink>
-              </FooterLinkGroup>
-            </div>
-            <div>
-              <FooterTitle title="Follow us" />
-              <FooterLinkGroup col>
-                <FooterLink href="#">Github</FooterLink>
-                <FooterLink href="#">Discord</FooterLink>
-              </FooterLinkGroup>
-            </div>
-            <div>
-              <FooterTitle title="Legal" />
-              <FooterLinkGroup col>
-                <FooterLink href="#">Privacy Policy</FooterLink>
-                <FooterLink href="#">Terms &amp; Conditions</FooterLink>
-              </FooterLinkGroup>
-            </div>
-          </div>
-        </div> */}
-        {/* <FooterDivider /> */}
+        <FooterDivider />
         <div className="w-full sm:flex sm:items-center sm:justify-between">
           <Image
             src="/logo.png"
-            width={90}
+            width={100}
             height={10}
             alt="Picture of the author"
           />
--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -1,6 +1,5 @@
 import Title from "./title";
 import { FaWhatsapp } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
 
 export default function LayoutFooter() {
   return (
@@ -21,14 +20,6 @@ export default function LayoutFooter() {
         <FaWhatsapp />
         اطلبي على الواتساب
       </a>
-      {/* <a
-        target="_blank"
-        href="https://www.facebook.com/groups/750706561027930/?ref=share&mibextid=NSMWBT"
-        className="w-fit mt-4 text-light bg-[#0064E0] border border-[#546E3C] 
-             rounded-4xl font-semi-bold text-md px-5 py-2.5 flex items-center gap-1 m-auto"
-      >
-        <FaFacebook /> جروب
-      </a> */}
     </div>
   );
 }
