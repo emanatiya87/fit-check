0b8ed603fdbb8c0eeab5ebc83f24b836c0cbf846
emanatiya87 <emanatiya87@gmail.com>
2026-02-21 10:16:00 +0200

    create home for separet layout

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,4 +1,6 @@
 import { CardComponent } from "./Card";
+import Title from "./title";
+import Link from "next/link";
 import { client, urlFor } from "@/lib/sanity";
 import { createImageUrlBuilder } from "@sanity/image-url";
 export const dynamic = "force-dynamic";
@@ -29,20 +31,29 @@ export default async function Products() {
   );
   console.log(products);
   return (
-    <div className="w-[85vw] m-auto flex flex-wrap items-start justify-center ">
-      {products.map((product: Product) => (
-        <CardComponent
-          key={product._id}
-          title={product.title}
-          price={product.price}
-          lastPrice={product.lastPrice ? product.lastPrice : ""}
-          imgSrc={
-            product.mainImage
-              ? urlFor(product.mainImage).url()
-              : "/placeholder.jpg"
-          }
-        />
-      ))}
-    </div>
+    <>
+      <Title titleText="Features" color="primary" />
+
+      <div className="w-[85vw] m-auto flex flex-wrap items-start justify-center ">
+        {products.map((product: Product) => (
+          <Link
+            href={`/product/${product._id}`}
+            key={product._id}
+            className="w-1/2 sm:w-1/4 md:w-1/5 px-2 mb-3"
+          >
+            <CardComponent
+              title={product.title}
+              price={product.price}
+              lastPrice={product.lastPrice ? product.lastPrice : ""}
+              imgSrc={
+                product.mainImage
+                  ? urlFor(product.mainImage).url()
+                  : "/placeholder.jpg"
+              }
+            />
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
