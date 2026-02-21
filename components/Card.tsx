e1aed4680f723d399fe0982f68ee831f6b8652c3
emanatiya87 <emanatiya87@gmail.com>
2026-02-21 03:02:08 +0200

    card dynamic

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,16 @@
 import { FaFire } from "react-icons/fa";
 import { CiHeart } from "react-icons/ci";
-
-export function CardComponent() {
+export function CardComponent({
+  imgSrc,
+  title,
+  lastPrice,
+  price,
+}: {
+  imgSrc: string;
+  title: string;
+  lastPrice: string;
+  price: string;
+}) {
   return (
     <div className="w-1/2  sm:w-1/4 md:w-1/5 px-2 mb-3">
       <div className="block shadow-2xl rounded-2xl ">
@@ -11,8 +20,8 @@ export function CardComponent() {
           </button>
           <img
             className="transition-transform duration-300 hover:scale-110"
-            src="/cardkin.jpg"
-            alt="cardijan"
+            src={imgSrc}
+            alt={title}
           />
         </div>
         <div className="py-2 px-1 text-center gap-2 flex flex-col justify-center items-center">
@@ -21,14 +30,14 @@ export function CardComponent() {
             Trending
           </div>
           <h5 className="border-b-gray-300  border pb-2 border-t-0 border-x-0 text-sm font-semibold tracking-tight text-gray-900 dark:text-white ">
-            كارديجان رمضان | Cardigan
+            {title}
           </h5>
           <div className="flex items-center justify-center">
             <span className="me-2 text-md text-gray-500 line-through dark:text-white">
-              $599
+              {lastPrice}
             </span>
             <span className="text-md font-semibold text-gray-900 dark:text-white">
-              $599
+              {price}
             </span>
           </div>
         </div>
--- components/Products.tsx
+++ components/Products.tsx
@@ -2,13 +2,12 @@ import { CardComponent } from "./Card";
 export default function Products() {
   return (
     <div className="w-[85vw] m-auto  flex flex-wrap items-center content-center">
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
-      <CardComponent />
+      <CardComponent
+        title=" كارديجان رمضان | Cardigan"
+        price="400"
+        lastPrice="500"
+        imgSrc="/cardkin.jpg"
+      />
     </div>
   );
 }
