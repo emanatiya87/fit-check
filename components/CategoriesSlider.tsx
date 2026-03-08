54b9560cf31934e658c6e5ea5c17fed7f769f3e5
emanatiya87 <emanatiya87@gmail.com>
2026-03-08 12:21:59 +0200

    colors name

--- components/CategoriesSlider.tsx
+++ components/CategoriesSlider.tsx
@@ -83,7 +83,7 @@ export default function CategoriesSliderAutoSnap() {
             className="shrink-0 w-1/2 sm:w-1/2 md:w-1/4 lg:w-1/4 snap-start"
           >
             <Link href={category.href}>
-              <div className="relative w-full h-48 md:h-56 rounded-xl overflow-hidden bg-gray-100">
+              <div className="relative w-full h-70 md:h-56 rounded-xl overflow-hidden bg-gray-100">
                 <Image
                   src={category.image}
                   alt={category.name}
