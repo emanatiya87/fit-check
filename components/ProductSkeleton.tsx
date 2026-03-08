2e1080cfe57ac22244788e5d7df468407511a8f7
emanatiya87 <emanatiya87@gmail.com>
2026-03-08 10:31:55 +0200

    skelton of loadin products

--- components/ProductSkeleton.tsx
+++ components/ProductSkeleton.tsx
@@ -1,21 +1,24 @@
+import Title from "./title";
 export default function ProductSkeleton() {
-  // هنعمل مصفوفة وهمية من 4 عناصر عشان نرسم شكل الكروت
-  const skeletons = [1, 2, 3, 4];
+  const skeletons = [1, 2, 3, 4, 5];
 
   return (
-    <div className="w-[85vw] m-auto flex flex-wrap items-start justify-center gap-4 py-10">
-      {skeletons.map((i) => (
-        <div
-          key={i}
-          className="w-62.5 h-87.5 bg-gray-200 animate-pulse rounded-xl"
-        >
-          <div className="w-full h-2/3 bg-gray-300 rounded-t-xl"></div>
-          <div className="p-4 space-y-3">
-            <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-            <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+    <>
+      <Title titleText="New Products" color="primary" />
+      <div className="md:w-[85vw] w-[95%] m-auto flex flex-wrap items-start justify-center ">
+        {skeletons.map((i) => (
+          <div
+            key={i}
+            className="w-1/2 sm:w-1/4 md:w-1/5 px-2 mb-3 h-87.5 bg-gray-200 animate-pulse rounded-xl"
+          >
+            <div className="w-full h-2/3 bg-gray-300 rounded-t-xl"></div>
+            <div className="p-4 space-y-3">
+              <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+              <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
